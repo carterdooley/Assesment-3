fccdd3ad8549f70612c13f5b4847927d5fc8365a
Carter Dooley <carter.j.dooley43@gmail.com>
2021-10-30 10:09:12 -0500

    Finished Part 5

---
+++ profile.js
@@ -0,0 +1,14 @@
+//Code
+
+document.querySelector('#color').addEventListener('click',() => {
+    alert("My favorite color is Purple")
+})
+
+document.querySelector('#place').addEventListener('click', () => {
+    alert('My Favorite place is The middle of the Gulf of Mexico')
+})
+
+
+document.querySelector('#ritual').addEventListener('click', () => {
+    alert('My favorite ritual is going to bed')
+})
